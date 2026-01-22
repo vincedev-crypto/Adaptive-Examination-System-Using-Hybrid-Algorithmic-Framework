a2781bf7b6de66988a35209c57fceeac2f952170
Vincent James Contento <vincentjames.contento.mnl@eac.edu.ph>
2026-01-22 10:44:41 +0800

    Application Changes

--- src/main/java/com/exam/algo/Model/AlgoApplication.java
+++ src/main/java/com/exam/algo/Model/AlgoApplication.java
@@ -2,8 +2,12 @@ package com.exam.algo.Model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.exam", "Service"})
+@EnableJpaRepositories(basePackages = "com.exam.repository")
 public class AlgoApplication {
 
 	public static void main(String[] args) {
