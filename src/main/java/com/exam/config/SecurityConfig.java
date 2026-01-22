062c5a9c6303694ed53e36442bfae8791e012a02
Vincent James Contento <vincentjames.contento.mnl@eac.edu.ph>
2026-01-22 10:29:29 +0800

    SecurityConfig Fixation

--- src/main/java/com/exam/config/SecurityConfig.java
+++ src/main/java/com/exam/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/login", "/register", "/css/**", "/js/**", "/images/**").permitAll()
                 .requestMatchers("/teacher/**", "/homepage", "/process-exams", "/enroll-student", "/distribute").hasAuthority("TEACHER")
